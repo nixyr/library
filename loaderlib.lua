d35c8c7de64cb982f9c7645b2c16893fb9e4665d
nixyr <105905969+nixyr@users.noreply.github.com>
2022-06-02 03:21:52 +0800

    Add files via upload

--- loaderlib.lua
+++ loaderlib.lua
@@ -104,7 +104,7 @@ LoaderTitle.TextColor3 = Color3.fromRGB(255, 255, 255)
 LoaderTitle.Font = Enum.Font.GothamBold
 LoaderTitle.Position = UDim2.new(0, 17, 0, 50)
 
-function Nixyr:Button(btext,callback)
+function Nix:Button(btext,callback)
 local Button = Instance.new("TextButton")
 local ButtonCorner = Instance.new("UICorner")
 
